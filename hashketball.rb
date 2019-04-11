a90d8132a2df781f698afc271f0654b9fec80541
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-11 14:47:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,6 +154,7 @@ end
 
 def big_shoe_rebounds
   player_hash = game_hash[:home][:players].merge(game_hash[:away][:players])
-  binding.pry
-  big_shoe = player_hash.map {|player| player.fetch(:shoe)}
 end
+
+def get_shoe
+  player_hash.map {|player| player.fetch(:shoe) == shoe}
